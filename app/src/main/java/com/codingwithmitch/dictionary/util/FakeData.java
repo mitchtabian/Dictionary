6895a53fa7e5331668ef823ef1207ee0acde02f4
Mitch Tabian <mitch@tabian.ca>
2018-10-26 13:01:13 -0700

    starting point

--- app/src/main/java/com/codingwithmitch/dictionary/util/FakeData.java
+++ app/src/main/java/com/codingwithmitch/dictionary/util/FakeData.java
@@ -5,18 +5,18 @@ import com.codingwithmitch.dictionary.models.Word;
 public class FakeData {
 
     public static final Word[] words = {
-            new Word("Word #1", "Some definition for word 1", "Oct 13, 2018"),
-            new Word("Word #2", "Some definition for word 2", "Oct 14, 2018"),
-            new Word("Word #3", "Some definition for word 3", "Oct 15, 2018"),
-            new Word("Word #4", "Some definition for word 4", "Oct 16, 2018"),
-            new Word("Word #5", "Some definition for word 5", "Oct 17, 2018"),
-            new Word("Word #6", "Some definition for word 6", "Oct 18, 2018"),
-            new Word("Word #7", "Some definition for word 7", "Oct 19, 2018"),
-            new Word("Word #8", "Some definition for word 8", "Oct 20, 2018"),
-            new Word("Word #9", "Some definition for word 9", "Oct 21, 2018"),
-            new Word("Word #10", "Some definition for word 10", "Oct 22, 2018"),
-            new Word("Word #11", "Some definition for word 11", "Oct 23, 2018"),
-            new Word("Word #12", "Some definition for word 12", "Oct 24, 2018"),
-            new Word("Word #13", "Some definition for word 13", "Oct 25, 2018"),
+            new Word("Word #1", "Some definition for word 1", "10-2018"),
+            new Word("Word #2", "Some definition for word 2", "10-2018"),
+            new Word("Word #3", "Some definition for word 3", "10-2018"),
+            new Word("Word #4", "Some definition for word 4", "10-2018"),
+            new Word("Word #5", "Some definition for word 5", "10-2018"),
+            new Word("Word #6", "Some definition for word 6", "10-2018"),
+            new Word("Word #7", "Some definition for word 7", "10-2018"),
+            new Word("Word #8", "Some definition for word 8", "10-2018"),
+            new Word("Word #9", "Some definition for word 9", "10-2018"),
+            new Word("Word #10", "Some definition for word 10", "10-2018"),
+            new Word("Word #11", "Some definition for word 11", "10-2018"),
+            new Word("Word #12", "Some definition for word 12", "10-2018"),
+            new Word("Word #13", "Some definition for word 13", "10-2018"),
     };
 }
