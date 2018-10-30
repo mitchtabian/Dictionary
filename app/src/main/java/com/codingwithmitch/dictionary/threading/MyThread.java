16deff01970ce0287d4e5e231bd4c115244b23fe
Mitch Tabian <mitch@tabian.ca>
2018-10-29 17:47:15 -0700

    start

--- app/src/main/java/com/codingwithmitch/dictionary/threading/MyThread.java
+++ app/src/main/java/com/codingwithmitch/dictionary/threading/MyThread.java
@@ -1,13 +1,13 @@
 package com.codingwithmitch.dictionary.threading;
 
+
 import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 
-import com.codingwithmitch.dictionary.models.Word;
 import com.codingwithmitch.dictionary.util.Constants;
+
 
 public class MyThread extends Thread {
 
@@ -57,11 +57,12 @@ public class MyThread extends Thread {
                 case Constants.WORD_INSERT_NEW:{
                     Log.d(TAG, "handleMessage: saving word on thread: " + Thread.currentThread().getName());
 
-//                    Message message = Message.obtain(mMainThreadHandler, Constants.WORD_INSERT_SUCCESS);
+//                    Message message = Message.obtain(mMainThreadHandler, Constants.WORDS_RETRIEVE_SUCCESS);
 //                    message.sendToTarget();
 
-                    Message message = Message.obtain(null, Constants.WORD_INSERT_SUCCESS);
+                    Message message = Message.obtain(null, Constants.WORDS_RETRIEVE_SUCCESS);
                     mMainThreadHandler.sendMessage(message);
+
 
                     break;
                 }
@@ -69,20 +70,17 @@ public class MyThread extends Thread {
                 case Constants.WORD_UPDATE:{
                     Log.d(TAG, "handleMessage: updating word on thread: " + Thread.currentThread().getName());
 
-
                     break;
                 }
 
                 case Constants.WORDS_RETRIEVE:{
                     Log.d(TAG, "handleMessage: retrieving words on thread: " + Thread.currentThread().getName());
 
-
                     break;
                 }
 
                 case Constants.WORD_DELETE:{
                     Log.d(TAG, "handleMessage: deleting word on thread: " + Thread.currentThread().getName());
-
 
                     break;
                 }
