b834973e97b3fb23fec35837d0f17a86f1b76b43
Mitch Tabian <mitch@tabian.ca>
2018-10-29 17:50:39 -0700

    end

--- app/src/main/java/com/codingwithmitch/dictionary/threading/MyThread.java
+++ app/src/main/java/com/codingwithmitch/dictionary/threading/MyThread.java
@@ -1,13 +1,13 @@
 package com.codingwithmitch.dictionary.threading;
 
+
 import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 
-import com.codingwithmitch.dictionary.models.Word;
 import com.codingwithmitch.dictionary.util.Constants;
+
 
 public class MyThread extends Thread {
 
@@ -15,7 +15,7 @@ public class MyThread extends Thread {
 
     private MyThreadHandler mMyThreadHandler = null;
     private Handler mMainThreadHandler = null;
-    private boolean isRunning = false;
+    private boolean isRunning;
 
     public MyThread(Handler mMainThreadHandler) {
         this.mMainThreadHandler = mMainThreadHandler;
@@ -66,11 +66,12 @@ public class MyThread extends Thread {
                 case Constants.WORD_INSERT_NEW:{
                     Log.d(TAG, "handleMessage: saving word on thread: " + Thread.currentThread().getName());
 
-//                    Message message = Message.obtain(mMainThreadHandler, Constants.WORD_INSERT_SUCCESS);
+//                    Message message = Message.obtain(mMainThreadHandler, Constants.WORDS_RETRIEVE_SUCCESS);
 //                    message.sendToTarget();
 
-                    Message message = Message.obtain(null, Constants.WORD_INSERT_SUCCESS);
+                    Message message = Message.obtain(null, Constants.WORDS_RETRIEVE_SUCCESS);
                     mMainThreadHandler.sendMessage(message);
+
 
                     break;
                 }
@@ -78,20 +79,17 @@ public class MyThread extends Thread {
                 case Constants.WORD_UPDATE:{
                     Log.d(TAG, "handleMessage: updating word on thread: " + Thread.currentThread().getName());
 
-
                     break;
                 }
 
                 case Constants.WORDS_RETRIEVE:{
                     Log.d(TAG, "handleMessage: retrieving words on thread: " + Thread.currentThread().getName());
 
-
                     break;
                 }
 
                 case Constants.WORD_DELETE:{
                     Log.d(TAG, "handleMessage: deleting word on thread: " + Thread.currentThread().getName());
-
 
                     break;
                 }
