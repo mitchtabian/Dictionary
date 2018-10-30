017c366844409bcb1df9cdfc0526ddefcbe1f821
Mitch Tabian <mitch@tabian.ca>
2018-10-29 17:19:14 -0700

    end

--- app/src/main/java/com/codingwithmitch/dictionary/threading/MyThread.java
+++ app/src/main/java/com/codingwithmitch/dictionary/threading/MyThread.java
@@ -1,12 +1,13 @@
 package com.codingwithmitch.dictionary.threading;
 
+
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 
-import com.codingwithmitch.dictionary.models.Word;
 import com.codingwithmitch.dictionary.util.Constants;
+
 
 public class MyThread extends Thread {
 
