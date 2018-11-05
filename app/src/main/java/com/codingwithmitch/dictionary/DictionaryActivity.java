8517bf7064880d197bb6e4850d473ec61d90b7dc
Mitch Tabian <mitch@tabian.ca>
2018-11-05 08:40:54 -0800

    start

--- app/src/main/java/com/codingwithmitch/dictionary/DictionaryActivity.java
+++ app/src/main/java/com/codingwithmitch/dictionary/DictionaryActivity.java
@@ -283,6 +283,8 @@ public class DictionaryActivity extends AppCompatActivity implements
     }
 
     private void executeThreadPool(){
+		clearWords();
+		
         int numTasks = Runtime.getRuntime().availableProcessors();
 
         int chunkSize = (mNumRows % numTasks) != 0 ?
